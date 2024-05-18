22107293b8e75ded95cbe7ea11b959f0471d97da
imber <a1157911285@163.com>
2024-05-18 17:13:10 +0800

    chore: 调节图片盒子的尺寸

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default function Page(): JSX.Element {
     <div className="flex justify-center flex-col lg:pt-40 pt-8 pb-10">
       <div className="text-4xl text-center mb-8">Demos</div>
       <div className="mx-auto grid grid-cols-1 font-bold w-[90vw] gap-4 lg:grid-cols-3">
-        <div className="border border-gray-400 rounded-lg overflow-hidden">
+        <div className="rounded-lg overflow-hidden border border-gray-400">
           <Link href="/fade-in">
-            <div className="w-full h-[45vw] lg:h-[calc((100vw_-_20px)/3/2)]">
+            <div className="w-full h-[45vw] lg:h-[calc((90vw_-_32px_-_6px)/3/2)]">
               <Image
                 src="/fade-in.gif"
                 width={600}
@@ -21,9 +21,9 @@ export default function Page(): JSX.Element {
           </Link>
         </div>
 
-        <div className="border border-gray-400 rounded-lg overflow-hidden">
+        <div className="rounded-lg overflow-hidden border border-gray-400">
           <Link href="/fade-in">
-            <div className="w-full h-[45vw] lg:h-[calc((100vw_-_20px)/3/2)]">
+            <div className="w-full h-[45vw] lg:h-[calc((90vw_-_32px_-_6px)/3/2)]">
               <Image
                 src="/fade-in.gif"
                 width={600}
@@ -36,9 +36,9 @@ export default function Page(): JSX.Element {
           </Link>
         </div>
 
-        <div className="border border-gray-400 rounded-lg overflow-hidden">
+        <div className="rounded-lg overflow-hidden border border-gray-400">
           <Link href="/fade-in">
-            <div className="w-full h-[45vw] lg:h-[calc((100vw_-_20px)/3/2)]">
+            <div className="w-full h-[45vw] lg:h-[calc((90vw_-_32px_-_6px)/3/2)]">
               <Image
                 src="/fade-in.gif"
                 width={600}
