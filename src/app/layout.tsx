795a307d26b028b1d951eb95e21b01fe01486838
imber <a1157911285@163.com>
2024-06-07 15:00:31 +0800

    chore: html文档改为zh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="zh" suppressHydrationWarning>
       <link rel="canonical" href="https://imber.top"></link>
       <body suppressHydrationWarning>
         <ThemeProvider
