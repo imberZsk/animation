40522628d7372e3a6d8b0d3a851bbc3b5e1e3010
imber <a1157911285@163.com>
2024-06-18 11:19:21 +0800

    chore: unoptimized

--- src/app/@modal/(.)animations/[id]/page.tsx
+++ src/app/@modal/(.)animations/[id]/page.tsx
@@ -6,7 +6,14 @@ import { Modal } from './modal'
 export default function Page({ params: { id } }: { params: { id: string } }) {
   return (
     <Modal>
-      <Image src={`/${id}.gif`} width={600} height={400} alt="normal" className="h-full w-full object-cover" />
+      <Image
+        src={`/${id}.gif`}
+        width={600}
+        height={400}
+        alt="normal"
+        className="h-full w-full object-cover"
+        unoptimized
+      />
     </Modal>
   )
 }
