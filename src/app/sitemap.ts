38128bb013cbb5de7b8b9fd5a63b92f73ae94f76
imber <a1157911285@163.com>
2024-08-01 23:46:36 +0800

    feat: 自动生成sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,24 @@
-import { MetadataRoute } from 'next'
+import { globby } from 'globby'
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://imber.top',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1
-    }
-  ]
+function addPage(page: string) {
+  const path = page.replace('src/app', '').replace('.tsx', '').replace('.mdx', '').replace('/page', '')
+  return path
+}
+
+export default async function sitemap() {
+  const pages = await globby([
+    'src/app/**/*.tsx',
+    '!src/app/_*.js',
+    '!src/app/{sitemap,layout}.{js,jsx,ts,tsx}',
+    '!src/app/api',
+    '!src/app/@modal/**'
+  ])
+
+  const routes = pages.map((page: string) => ({
+    url: `${process.env.WEBSITE_URL}${addPage(page)}`,
+    // ISO标准
+    lastModified: new Date().toISOString()
+  }))
+
+  return [...routes]
 }
